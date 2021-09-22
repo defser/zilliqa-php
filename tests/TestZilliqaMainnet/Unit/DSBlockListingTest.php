01df50fb1834fe28944689ec0a599070330aa409
patrickfalize <info@defser.nl>
2021-09-22 20:24:32 +0200

    Add test DSBlockListingTest

---
+++ tests/TestZilliqaMainnet/Unit/DSBlockListingTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace TestZilliqaMainnet\Unit;
+
+use Zilliqa\DataType\ZilliqaNumber;
+use TestZilliqaMainnet\TestZilliqaClient;
+
+/**
+ *
+ * @ingroup zilliqaTests
+ */
+class DSBlockListingTest extends TestZilliqaClient
+{
+    public function testMainNetDSBlockListing() {
+        $pageNumber = new ZilliqaNumber(1);
+        $DSBlockListings = $this->web3->DSBlockListing($pageNumber);
+        $this->assertGreaterThanOrEqual(1452, $DSBlockListings->maxPages->val());
+        $this->assertGreaterThanOrEqual(14514, $DSBlockListings->data[0]->BlockNum->val());
+        $this->assertIsString($DSBlockListings->data[0]->Hash->val());
+        $this->assertEquals(64, strlen($DSBlockListings->data[0]->Hash->val()));
+    }
+}
