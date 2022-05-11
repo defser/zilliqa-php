231eb5889b252707c3acb214da3d468b2ecaf889
patrickfalize <info@defser.nl>
2022-05-11 10:37:35 +0200

    Composer update & assert balance is greater than 1 in testMainNetGetBalance

--- tests/TestZilliqaMainnet/Unit/GetBalanceTest.php
+++ tests/TestZilliqaMainnet/Unit/GetBalanceTest.php
@@ -13,9 +13,9 @@ class GetBalanceTest extends TestZilliqaClient
 {
     public function testMainNetGetBalance() {
         // @see https://viewblock.io/zilliqa/address/zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz
-        $mainNetTx = new ZilliqaBech32('zil1l4khjax278mae63ywsv3l4uj8gg4fsxh34nnzh');
+        $mainNetTx = new ZilliqaBech32('zil1wyyyr29eg7qw3mys6vp86l2808vy4yl2yw389x');
         $balance = $this->web3->GetBalance($mainNetTx);
-        $this->assertSame(100000000.0, ZilliqaStatic::convertCurrency($balance->balance));
+        $this->assertGreaterThan(1, ZilliqaStatic::convertCurrency($balance->balance));
         $this->assertIsArray($balance->balance->toArray());
         $this->assertIsArray($balance->toArray());
         $this->assertArrayHasKey('balance', $balance->toArray());
